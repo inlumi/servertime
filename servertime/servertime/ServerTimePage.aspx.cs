0a1345df4dacabe42bae6c9ce087340ab78d1b36
Maria <marie.valerievna@gmail.com>
2015-07-22 23:28:42 +0300

    Basic empty project

---
+++ servertime/servertime/ServerTimePage.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace servertime
+{
+    public partial class ServerTimePage : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
